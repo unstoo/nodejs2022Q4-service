9e21c05619d5b168c85508f8c0cecc54ecce52ac
Jevgenijs Kozlovs <unstoo@gmail.com>
2023-02-06 23:31:00 +0200

    feat: use dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
@@ -5,6 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
